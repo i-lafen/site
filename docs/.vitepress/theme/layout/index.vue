d4414631272f982de128998907b1efcd5675e8f6
i-lafen <331462136@qq.com>
2024-09-13 21:26:44 +0800

    feat: animation

---
+++ docs/.vitepress/theme/layout/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import useSpendTime from '../hooks/useSpendTime'
+import useThemeTransition from '../hooks/useThemeTransition'
+import './index.css'
+
+const { text, textStyle, colorStyle } = useSpendTime()
+useThemeTransition()
+</script>
+
+<template>
+  <DefaultTheme.Layout>
+    <template #doc-before>
+      <span :style="textStyle">
+        ⏰
+        <span :style="colorStyle">{{ text }}</span>
+      </span>
+    </template>
+  </DefaultTheme.Layout>
+</template>
