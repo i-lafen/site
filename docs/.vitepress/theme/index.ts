1dab2e7dbab91c34335d051a7292e4e9e8b79da3
i-lafen <331462136@qq.com>
2024-09-13 21:27:10 +0800

    feat: theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,18 +1,14 @@
 import DefaultTheme from 'vitepress/theme'
-import './index.css'
-import MyLayout from './MyLayout.vue'
 
-import { onMounted, watch, nextTick } from 'vue'
-import { useRoute } from 'vitepress'
-import mediumZoom from 'medium-zoom'
+import './index.css'
+import Layout from './layout/index.vue'
+
+import useImgZoom from './hooks/useImgZoom'
 
 export default {
   ...DefaultTheme,
-  Layout: MyLayout,
+  Layout,
   setup() {
-    const route = useRoute()
-    const initRoom = () => mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
-    onMounted(initRoom)
-    watch(() => route.path, () => nextTick(initRoom))
+    useImgZoom()
   }
 }
