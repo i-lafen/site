c9089febbc3514ee177b472f6e231863f0d26952
i-lafen <331462136@qq.com>
2024-10-09 21:30:04 +0800

    feat: build

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,12 +3,7 @@ import DefaultTheme from 'vitepress/theme'
 import './index.css'
 import Layout from './layout/index.vue'
 
-import useImgZoom from './hooks/useImgZoom'
-
 export default {
   ...DefaultTheme,
   Layout,
-  setup() {
-    useImgZoom()
-  }
 }
